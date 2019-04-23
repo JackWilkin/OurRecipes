460a3fbe59ac358608baacc6de6e593866172591
Jack Wilkin <wilkin.j@husky.neu.edu>
2019-04-22 21:47:02 -0400

    Updates

---
+++ precache-manifest.aa05332bd3af53b90a3333ca15025da7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/OurRecipes/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "00c0d1bc1bff183d53b9",
+    "url": "/OurRecipes/static/css/main.e95ebfe4.chunk.css"
+  },
+  {
+    "revision": "c5c24aaab050e407dadc",
+    "url": "/OurRecipes/static/js/runtime~main.78590181.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/OurRecipes/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "9a070b354a6148f591ac",
+    "url": "/OurRecipes/static/js/2.8f4bea6f.chunk.js"
+  },
+  {
+    "revision": "ca95d0919eacfaea7b799bd562c60495",
+    "url": "/OurRecipes/static/media/mixer-white.ca95d091.png"
+  },
+  {
+    "revision": "51723af10364e2bb86880ee3ae6a710c",
+    "url": "/OurRecipes/static/media/pasta.51723af1.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/OurRecipes/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/OurRecipes/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/OurRecipes/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/OurRecipes/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/OurRecipes/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "00c0d1bc1bff183d53b9",
+    "url": "/OurRecipes/static/js/main.dc643135.chunk.js"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/OurRecipes/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/OurRecipes/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/OurRecipes/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/OurRecipes/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/OurRecipes/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/OurRecipes/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/OurRecipes/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/OurRecipes/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/OurRecipes/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "9a070b354a6148f591ac",
+    "url": "/OurRecipes/static/css/2.f5ce03ab.chunk.css"
+  },
+  {
+    "revision": "20b7af53d6e7a0f5afa57d821d85624f",
+    "url": "/OurRecipes/index.html"
+  }
+];
